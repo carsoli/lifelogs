afe4c7d4a6439b627804c87bd1249ac331de63ca
carsoli <caroleemad96@gmail.com>
2018-07-20 18:22:00 +0200

    comments moved around (trivial)

--- src/application/tasks/PreloadFramesTask.java
+++ src/application/tasks/PreloadFramesTask.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.util.ArrayList;
 
 import application.controller.FramesGliderController;
-import application.structs.FramesBufferController;
+import application.controller.FramesBufferController;
 import application.utils.Constants;
 import application.view.ViewUtils;
 import javafx.concurrent.Task;
