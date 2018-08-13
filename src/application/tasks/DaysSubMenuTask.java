7ac5118fc692a2b1c6b6ee61f995a22da1f335c7
carsoli <caroleemad96@gmail.com>
2018-08-13 23:09:15 +0200

    modified UI set on succeedded and order of tasks execution to enable loading multiple users' days, or different users

--- src/application/tasks/DaysSubMenuTask.java
+++ src/application/tasks/DaysSubMenuTask.java
@@ -9,6 +9,7 @@ import application.controller.FramesBufferController;
 import application.structs.Participant;
 import application.utils.Constants;
 import application.utils.ImagesToVideoConverter;
+import application.utils.SerialComm;
 import application.view.FramesGlider;
 import application.view.VideoPlayer;
 import application.view.ViewUtils;
@@ -21,75 +22,81 @@ import javafx.scene.control.MenuItem;
 
 @SuppressWarnings("rawtypes")
 public class DaysSubMenuTask extends Task{
-	private Menu createVideosSM = new Menu();
 	private ArrayList<Participant> pArrayList = new ArrayList<Participant>();
-	private ArrayList<Day> dArrayList = new ArrayList<Day>();
 
-	public DaysSubMenuTask(Menu createVideosSM, ArrayList<Participant> pArrayList, 
-			ArrayList<Day> dArrayList) {
-		this.createVideosSM = createVideosSM;
+	public DaysSubMenuTask(ArrayList<Participant> pArrayList) {
 		this.pArrayList = pArrayList;
-		this.dArrayList = dArrayList;
 	}
 	
 	@Override
 	protected Object call() throws Exception {
-		initializeDaysSubMenu(this.createVideosSM, this.pArrayList, this.dArrayList);
+		initializeDaysSubMenu(
+				this.pArrayList);
 		return null;
 	}
 	
 	@SuppressWarnings("unchecked")
-	private static void initializeDaysSubMenu(Menu createVideosSM, 
-			ArrayList<Participant> pArrayList, ArrayList<Day> dArrayList) {
+	private static void initializeDaysSubMenu(
+			ArrayList<Participant> pArrayList) {
+		
+		Menu createVideosSM = ViewUtils.getCreateVideoSM();
+		
 		//create a days sub menu based on the choice of participant
-		createVideosSM.getItems().clear();
+		createVideosSM.getItems().clear(); //reset in case it was already filled
 		int chosenP = MainController.getChosenP(); 
 		if(chosenP > -1 && pArrayList != null) {
-			dArrayList = (pArrayList.get(chosenP)).getDays();
+			ArrayList<Day> dArrayList = (pArrayList.get(chosenP)).getDays();
 			for (Day d: dArrayList) {
 				MenuItem dItem = new MenuItem(d.getName());
 				int dayIdx = dArrayList.indexOf(d);
 				dItem.setId("" + dayIdx);
-				dItem.setDisable(true);//when the day's entirely loaded, it'll be enabled
+//				dItem.setDisable(true);//NO; disable when loading is taking place, in setOnAction
 				//thus allowing to start video creation
 				createVideosSM.getItems().add(dItem);
 				dItem.setOnAction(e -> {
+					//close Arduino port; reopened during framesGliderInitialization call
+					SerialComm.disconnectArduino();
 					//until the curr vid is created, don't change the chosenP bc it's a global variable
 					ViewUtils.getChooseParticipantSM().setDisable(true); 
+					//disable until the chosen day's video is created
 					ViewUtils.getCreateVideoSM().setDisable(true);
 					MainController.setChosenD(dayIdx); 
+					
 					//============COMMENT WHEN YOU DON'T NEED THE VIDEOS TAB
-//					Task createVideoTask = new CreateVideoTask(dayIdx);
-//					MainController.addTask(createVideoTask);
-//					createVideoTask.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-//						@Override
-//						public void handle(WorkerStateEvent event) {
-//			                Platform.runLater(new Runnable() {
-//		                        @Override
-//		                        public void run() {
-//			                    	ImagesToVideoConverter.EncodeVideoAndClose();
-//			                    	ViewUtils.getChooseParticipantSM().setDisable(false);
-//			                    	ViewUtils.getCreateVideoSM().setDisable(false);
-//			                    	VideoPlayer.postProcessVideo();
-//		                        }
-//			                });
-//						}
-//					});
-//					MainController.execute(createVideoTask);
+					Task createVideoTask = new CreateVideoTask(dayIdx);
+					MainController.addTask(createVideoTask);
+					createVideoTask.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+						@Override
+						public void handle(WorkerStateEvent event) {
+			                Platform.runLater(new Runnable() {
+		                        @Override
+		                        public void run() {
+			                    	ImagesToVideoConverter.EncodeVideoAndClose();
+			                    	ViewUtils.getChooseParticipantSM().setDisable(false);
+			                    	ViewUtils.getCreateVideoSM().setDisable(false);
+			                    	VideoPlayer.postProcessVideo();
+		                        }
+			                });
+						}
+					});
+					MainController.execute(createVideoTask);
 					//=================================
 					//whenever we choose a different day, we need to reset LLI
 					//this way the preloadFramesTask can load things correctly
 					FramesGliderController.setLastLoadedFrame(0);
+
 					//and curr buff ptr because we decide to load while one video is still playing,
 					//curr buff ptr would be out of bound
-					FramesGlider.setCurrBuffPtr(0); //TODO: this should actually move to onSucceeded, not reset before loading while video is playing
+					//TODO: this should actually move to onSucceeded, not reset before loading while video is playing
+					FramesGlider.setCurrBuffPtr(0); 
+					
 					long startTime = System.nanoTime();
 					Task preloadFrames = new PreloadFramesTask();
 					MainController.addTask(preloadFrames);
 					preloadFrames.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
 						@Override
 						public void handle(WorkerStateEvent event) {
-			                Platform.runLater(new Runnable() {//Read Note in loadImagesTask.java
+			                Platform.runLater(new Runnable() {
 		                        @Override
 		                        public void run() {
 		                        	FramesGlider.initializeFrameGlider(
