2e5061302f8c3c2a9c9a94bfff0e907175e9ee50
carsoli <caroleemad96@gmail.com>
2018-08-13 23:09:56 +0200

    UI modifications for loading different participants, and task modifications

--- src/application/tasks/ParticipantSubMenuTask.java
+++ src/application/tasks/ParticipantSubMenuTask.java
@@ -3,8 +3,6 @@ package application.tasks;
 import java.util.ArrayList;
 
 import application.controller.MainController;
-import application.structs.DataSource;
-import application.structs.Day;
 import application.structs.Participant;
 import application.view.ViewUtils;
 import javafx.application.Platform;
@@ -17,10 +15,8 @@ import javafx.scene.control.MenuItem;
 @SuppressWarnings("rawtypes")
 public class ParticipantSubMenuTask extends Task{
 	//to track current user choices for directory, participant, and day
-	private static DataSource mDataSource = null;
-	private static ArrayList<Participant> pArrayList = null;
-	private static ArrayList<Day> dArrayList = null;
-	
+//	private static ArrayList<Participant> pArrayList = null;
+//	private static ArrayList<Day> dArrayList = null;
 	
 	@Override
 	protected Object call() throws Exception {
@@ -28,13 +24,10 @@ public class ParticipantSubMenuTask extends Task{
         return null; 
 	}
 
-	public static void setMainDataSource(DataSource ds) {
-		mDataSource = ds; 
-	}
 	
 	@SuppressWarnings( "unchecked" )
 	private static void initializeParticipantsSubMenu() {
-		pArrayList = mDataSource.getData();
+		ArrayList<Participant> pArrayList = MainController.getmDataSource().getData();
 		
 		if(pArrayList != null) {
 			ViewUtils.getChooseParticipantSM().getItems().clear(); //in case a new directory is chosen
@@ -49,33 +42,49 @@ public class ParticipantSubMenuTask extends Task{
 				ViewUtils.getChooseParticipantSM().getItems().add(pItem);
 				pItem.setOnAction(new EventHandler<ActionEvent>() {
 					public void handle(ActionEvent e) {
-						ViewUtils.getCreateVideoSM().setDisable(false);
+						//no - load the participant images, then enable video creation menu
+//						ViewUtils.getCreateVideoSM().setDisable(false); 
+
+						//prevent loading different participants until the first is done
+						ViewUtils.getChooseParticipantSM().setDisable(true);
+						ViewUtils.getCreateVideoSM().setDisable(true);
 
 						ViewUtils.getImagesTilePaneContainer().getChildren().clear();
-						MainController.setmDataSource(mDataSource);
-						Task loadImagesTask = new LoadImagesTask(mDataSource);	
-						MainController.addTask(loadImagesTask);
-						//prevent loading different images until the first is done (can't do it with threads)
-						ViewUtils.getChooseParticipantSM().setDisable(true);
+						Task loadImagesTask = new LoadImagesTask();	
+						
+						Task displayGridImagesTask = new DisplayGridImagesTask();
+						displayGridImagesTask.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+							public void handle(WorkerStateEvent event) {
+								Platform.runLater(() -> {
+									ViewUtils.getChooseParticipantSM().setDisable(false);
+									ViewUtils.getCreateVideoSM().setDisable(false);
+								});
+							}
+						});
 						loadImagesTask.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
 							 public void handle(WorkerStateEvent event) {
-					                Platform.runLater(new Runnable() {
-		                                @Override
-		                                public void run() {
-		                                	ViewUtils.getChooseParticipantSM().setDisable(false);
-		                                }
-		                            });
-					            }
-					        });       
-						MainController.execute(loadImagesTask);
-						//the execute call creates a new worker thread using the threadFactory method
-						//specified in the ExecutorService "threadPool"
+								MainController.addTask(displayGridImagesTask);
+								MainController.execute(displayGridImagesTask);
+							 }
+					    });       
 						int participantIdx = pArrayList.indexOf(p);
 						MainController.setChosenP(participantIdx);
-						ViewUtils.getCreateVideoSM().setDisable(false);
-						Task DSMTask = new DaysSubMenuTask(ViewUtils.getCreateVideoSM(), pArrayList, dArrayList);
+
+						Task DSMTask = new DaysSubMenuTask(pArrayList);
+						DSMTask.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+							@Override
+							public void handle(WorkerStateEvent event) {
+								MainController.addTask(loadImagesTask);
+								MainController.execute(loadImagesTask);								
+							}
+						});
 						MainController.addTask(DSMTask);
 						MainController.execute(DSMTask);
+						//ORDER OF EXECUTION
+						//DSM; updates createVideoSubMenu, then starts:
+						//loadImagesTask; which basically adds to the dataStructures of Day.FImages, Participant.AllImages
+						//then starts displayGridImagesTask; which replaces the empty HBoxes with the images
+						//then when done; enables both menus; chooseParticipant, createVideo
 					}
 				});
 			}
